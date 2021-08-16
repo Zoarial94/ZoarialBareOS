74bbea0b84171bbd3bbfa190ca45ed20800385af
Hunter White <HWB94@protonmail.com>
2021-08-16 00:55:32 -0400

    Created pic.h and pic.c
    These files have to do with the Programmable Interrupt Controller
    Functions include remapping IRQs, setting and setting ISR masks, etc

---
+++ src/arch/i686/pic.c
@@ -0,0 +1,89 @@
+#include <arch/i686/pic.h>
+#include <arch/i686/inline-asm.h>
+
+void PIC_remap(uint32_t offset1, uint32_t offset2) {
+    uint8_t a1, a2;
+ 
+	a1 = inb(PIC1_DATA);                        // save masks
+	a2 = inb(PIC2_DATA);
+ 
+	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+	io_wait();
+	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+	io_wait();
+	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
+	io_wait();
+	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
+	io_wait();
+	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	io_wait();
+	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+	io_wait();
+ 
+	outb(PIC1_DATA, ICW4_8086);
+	io_wait();
+	outb(PIC2_DATA, ICW4_8086);
+	io_wait();
+ 
+	outb(PIC1_DATA, a1);   // restore saved masks.
+	outb(PIC2_DATA, a2);
+}
+
+void IRQ_set_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);
+}
+
+void IRQ_clear_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);
+}
+
+static uint16_t PIC_get_irq_reg(uint32_t ocw3) {
+    /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
+     * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
+    outb(PIC1_COMMAND, ocw3);
+    outb(PIC2_COMMAND, ocw3);
+    return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
+}
+
+uint16_t PIC_get_irr(void) {
+    return PIC_get_irq_reg(PIC_READ_IRR);
+}
+
+uint16_t PIC_get_isr(void) {
+    return PIC_get_irq_reg(PIC_READ_ISR);
+}
+
+uint16_t PIC_get_mask(void) {
+    return inb(PIC1_DATA) | (inb(PIC2_DATA) << 8);
+}
+
+void PIC_set_mask(uint16_t mask) {
+    outb(PIC1_DATA, (uint8_t) mask);
+    outb(PIC2_DATA, (uint8_t) (mask >> 8));
+}
+
+void PIC_initialize(void) {
+    
+    PIC_remap(PIC1, PIC2);
+
+}
