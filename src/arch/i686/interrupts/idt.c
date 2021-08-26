55e39940c371826327696dd5a5b98d9663fa3524
Hunter White <HWB94@protonmail.com>
2021-08-26 16:44:09 -0400

    Added 32 exception handler stubs.
    Moved idt.c, idt.S, and pic.c into an interrupts directory.
    
    exception_handlers.S contains 32 stubs for the 32 cpu exceptions in the
    x86 standard. (Currently they only lock up the cpu).
    
    idt.c adds those stubs to the IDT.

---
+++ src/arch/i686/interrupts/idt.c
@@ -0,0 +1,35 @@
+#include <arch/i686/idt.h>
+#include <arch/i686/memory.h>
+#include <kernel/panic.h>
+#include <arch/i686/pic.h>
+
+extern uint64_t id_table[];
+extern void setIdt(void* idt_ptr, uint16_t idt_size);
+extern void isr_keyboard(void);
+extern void (*isr_exception_stub_table[32])(void);
+
+void IDT_add_descriptor(uint8_t index, void (*isr)(void), uint8_t flags) {
+    uint64_t *descriptor = (id_table + index);
+
+    *descriptor  = ((uint64_t)(uint32_t)isr << 32) & 0xFFFF000000000000;
+    *descriptor |= ((uint64_t)flags << 40);
+
+    *descriptor |= (uint32_t)isr & 0x0000FFFF;
+    *descriptor |= 0x00080000;
+
+}
+
+void IDT_initialize(void) {
+
+    // Add the 32 x86 defined exception handlers
+    // (Defined in exception_handlers.S)
+    for(int i = 0; i < 32; i++) {
+        IDT_add_descriptor(i, isr_exception_stub_table[i], 0x8E);
+    }
+    
+    // Add PS/2 keyboard handler
+    IDT_add_descriptor(PIC1_INT + 1, isr_keyboard, 0x8E);
+
+    // Set the IDT
+    setIdt(&id_table, IDT_SIZE*8);
+}
